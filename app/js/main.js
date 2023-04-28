70e8551d2f8fad026a34fae90c46881e1a7df0c4
Rodion <wead758@gmail.com>
2023-04-29 01:06:13 +0300

    added section video-fashion

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,14 @@
-$(function(){
-  $('.hero-slider__inner').slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplayspeed: 2000
-  });
+$(function () {
+    $('.hero-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 2000
+    });
+
+
+    Fancybox.bind("[data-fancybox]", {
+
+    });
 });
