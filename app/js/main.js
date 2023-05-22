d039385cfc01bdf3b4a95ae5ab2c2d6c013891b0
Rodion <wead758@gmail.com>
2023-05-22 23:24:22 +0300

    added blog main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,26 @@
 $(function () {
 
-//product page tabs
-$('.product-tabs__top-item').on('click', function (e) {
-    e.preventDefault();
-    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-    $(this).addClass('product-tabs__top-item--active');
+    //blog-page slider
 
-    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-    $($(this).attr('href')).addClass('product-tabs__content-item--active');  
-});
+    $('.blog-page__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 0.90625 6.382812 C 0.632812 6.722656 0.632812 7.277344 0.90625 7.621094 L 4.40625 11.996094 C 4.679688 12.335938 5.125 12.335938 5.398438 11.996094 C 5.671875 11.652344 5.671875 11.097656 5.398438 10.757812 L 2.390625 7 L 5.394531 3.242188 C 5.667969 2.902344 5.667969 2.347656 5.394531 2.003906 C 5.121094 1.664062 4.675781 1.664062 4.402344 2.003906 L 0.902344 6.378906 Z M 0.90625 6.382812 "/></g></svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="7px" height="14px" viewBox="0 0 7 14" version="1.1"><g><path d="M 6.09375 6.382812 C 6.367188 6.722656 6.367188 7.277344 6.09375 7.621094 L 2.59375 11.996094 C 2.320312 12.335938 1.875 12.335938 1.601562 11.996094 C 1.328125 11.652344 1.328125 11.097656 1.601562 10.757812 L 4.609375 7 L 1.605469 3.242188 C 1.332031 2.902344 1.332031 2.347656 1.605469 2.003906 C 1.878906 1.664062 2.324219 1.664062 2.597656 2.003906 L 6.097656 6.378906 Z M 6.09375 6.382812 "/></g></svg></button>',
+        infinite: false,
+    });
 
-   
-   
-   //product page slider
+    //product page tabs
+    $('.product-tabs__top-item').on('click', function (e) {
+        e.preventDefault();
+        $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+        $(this).addClass('product-tabs__top-item--active');
+
+        $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+        $($(this).attr('href')).addClass('product-tabs__content-item--active');
+    });
+
+
+
+    //product page slider
 
 
     $('.product-slide__thumb').slick({
@@ -27,20 +35,20 @@ $('.product-tabs__top-item').on('click', function (e) {
         asNavFor: '.product-slide__thumb',
         draggable: false,
         arrows: false,
-        fade: true    
+        fade: true
     });
 
     //grid/line-filter shop page
 
-    $('.shop-content__filter-btn').on('click', function (){
+    $('.shop-content__filter-btn').on('click', function () {
         $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
         $(this).addClass('shop-content__filter-btn--active');
     });
 
-    $('.button-list').on('click', function (){
+    $('.button-list').on('click', function () {
         $('.products-item').addClass('products-item--list');
     });
-    $('.button-grid').on('click', function (){
+    $('.button-grid').on('click', function () {
         $('.products-item').removeClass('products-item--list');
     });
 
@@ -88,7 +96,8 @@ $('.product-tabs__top-item').on('click', function (e) {
         starWidth: "17px",
         normalFill: "#ccccce",
         ratedFill: "#ffc35b",
-        readOnly: true
+        readOnly: true,
+        starSvg: ' <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px"viewBox="0 0 18 16" version="1.1">  <g id="surface1">    <path  d="M 9.902344 0.5625 C 9.738281 0.21875 9.386719 0 9.003906 0 C 8.617188 0 8.273438 0.21875 8.101562 0.5625 L 6.09375 4.695312 L 1.605469 5.359375 C 1.230469 5.414062 0.917969 5.679688 0.804688 6.039062 C 0.6875 6.398438 0.78125 6.792969 1.050781 7.058594 L 4.304688 10.28125 L 3.539062 14.835938 C 3.476562 15.210938 3.632812 15.589844 3.941406 15.8125 C 4.25 16.035156 4.660156 16.0625 4.996094 15.882812 L 9.007812 13.742188 L 13.015625 15.882812 C 13.351562 16.0625 13.761719 16.039062 14.070312 15.8125 C 14.382812 15.585938 14.539062 15.210938 14.476562 14.835938 L 13.703125 10.28125 L 16.960938 7.058594 C 17.226562 6.792969 17.324219 6.398438 17.207031 6.039062 C 17.085938 5.679688 16.777344 5.414062 16.402344 5.359375 L 11.914062 4.695312 Z M 9.902344 0.5625 " /></g></svg>'
     });
 
     //clock timer
@@ -99,7 +108,7 @@ $('.product-tabs__top-item').on('click', function (e) {
         const minutes = Math.floor((total / 1000 / 60) % 60);
         const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
         const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
         return {
             total,
             days,
@@ -108,36 +117,36 @@ $('.product-tabs__top-item').on('click', function (e) {
             seconds
         };
     }
-    
+
     function initializeClock(id, endtime) {
         const clock = document.querySelector('.promo__clock');
         const daysSpan = clock.querySelector('.promo__days');
         const hoursSpan = clock.querySelector('.promo__hours');
         const minutesSpan = clock.querySelector('.promo__minutes');
         const secondsSpan = clock.querySelector('.promo__seconds');
-    
+
         function updateClock() {
             const t = getTimeRemaining(endtime);
-    
+
             daysSpan.innerHTML = t.days;
             hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
             minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
             secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    
+
             if (t.total <= 0) {
                 clearInterval(timeinterval);
             }
         }
-    
+
         updateClock();
         const timeinterval = setInterval(updateClock, 1000);
     }
-    
+
     const deadline = $('.promo__clock').attr('data-time');
     initializeClock('promo__clock', deadline);
 
 
-  
+
 });
 
 
